b98e186738284b67996253bac7d4b07bc8d7e359
Khanh Truong <tmkhanh.it@gmail.com>
2023-11-28 12:06:46 +0700

    feat: test case

--- src/main.rs
+++ src/main.rs
@@ -21,3 +21,36 @@ async fn main() {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
+
+#[cfg(test)]
+mod tests {
+    use axum::body::Body;
+    use axum::http::{Request, StatusCode};
+    use http_body_util::BodyExt; // for `collect`
+    use serde_json::{json, Value};
+    use tower::ServiceExt; // for `call`, `oneshot`, and `ready`
+
+    use super::*;
+
+    fn send_get_request(uri: &str) -> Request<Body> {
+        Request::builder()
+            .uri(uri)
+            .method("GET")
+            .body(Body::empty())
+            .unwrap()
+    }
+
+    #[tokio::test]
+    async fn get_health_info() {
+        let app = get_routes();
+
+        let response = app.oneshot(send_get_request("/health/info")).await.unwrap();
+
+        // Check the response status code.
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!({ "status": "UP" }));
+    }
+}
